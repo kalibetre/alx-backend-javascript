72b85e9e9beca497ca449d4146987c81fc691a57
kalibetre <kalbetre@icloud.com>
2023-03-21 22:28:14 +0300

    remove debug console log

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((values) => {
-    console.log(values);
-  });
+  ]).then((values) => values);
 }
