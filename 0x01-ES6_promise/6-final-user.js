96b92610f3a7c7c135d41ea8fc16c9a5d02dc7e4
kalibetre <kalbetre@icloud.com>
2023-03-21 22:40:40 +0300

    fix return format

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((values) => values);
+  ]).then((values) => values.map((value) => ({
+    status: value.status,
+    value: value.value || value.reason,
+  })));
 }
--- 0x01-ES6_promise/tests/6-main.js
+++ 0x01-ES6_promise/tests/6-main.js
@@ -1,3 +1,10 @@
 import handleProfileSignup from '../6-final-user';
 
 console.log(handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
+
+const test = async () => {
+  const res = await handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg');
+  console.log(res);
+};
+
+test();
