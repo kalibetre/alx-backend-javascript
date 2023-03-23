2e0db85de935d049a8d8d845cd02747b95e79eea
kalibetre <kalbetre@icloud.com>
2023-03-23 21:54:42 +0300

    fix the tests for the Pricing class

--- 0x02-ES6_classes/tests/4-main.js
+++ 0x02-ES6_classes/tests/4-main.js
@@ -1,4 +1,6 @@
-import Currency from '../3-currency';
+import Currency from './3-currency.js';
+import Pricing from './4-pricing.js';
 
-const dollar = new Currency('$', 'Dollars');
-console.log(dollar.displayFullCurrency());
+const p = new Pricing(100, new Currency('EUR', 'Euro'));
+console.log(p);
+console.log(p.displayFullPrice());
