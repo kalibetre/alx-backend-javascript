33e561763192609030742ebc15c227fe1563530b
kalibetre <kalbetre@icloud.com>
2023-03-27 22:33:22 +0300

    throw error if api call reaches 5

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -8,7 +8,7 @@ export const weakMap = new WeakMap();
  */
 export function queryAPI(endpoint) {
   if (weakMap.get(endpoint)) {
-    if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+    if (weakMap.get(endpoint) === 4) throw new Error('Endpoint load is high');
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   } else {
     weakMap.set(endpoint, 1);
