483a519490672e6c0e2644ff737bbbdf373c0e80
kalibetre <kalbetre@icloud.com>
2023-03-27 22:12:17 +0300

    create a function that creates a Map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * Generate a map from a list of groceries
+ * @returns {Map}
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
