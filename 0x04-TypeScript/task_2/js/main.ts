5848b76145e3487d65ecf3886279f0f1c53dd963
kalibetre <kalbetre@icloud.com>
2023-03-29 21:14:49 +0300

    create a string literal type

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -47,3 +47,10 @@ function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) return employee.workDirectorTasks();
   else return employee.workTeacherTasks();
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') return 'Teaching Math';
+  else if (todayClass === 'History') return 'Teaching History';
+}
