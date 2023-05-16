2f573fe5cd2ac32a1005b0f1678ca2514e099c87
kalibetre <kalbetre@icloud.com>
2023-05-16 14:08:11 +0300

    printing to STDOUT

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * A function that prints in STDOUT the message passed as parameter
+ * @param message - the message to print
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
