a18794e0276c3dfd2f911b7538ecaa6454a51edd
kalibetre <kalbetre@icloud.com>
2023-05-16 14:15:23 +0300

    reading from stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+/**
+ * A program that prompts a user for a name and then displays it
+ * in STDOUT. The program also displays a closing message followed by a new
+ * line.
+ *
+ * Usage:
+ * $ node 1-stdin.js
+ * Welcome to Holberton School, what is your name?
+ * <name>
+ * Your name is: <name>
+ */
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write('This important software is now closing\n');
+  }
+});
